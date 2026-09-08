b356d5efd68aad665f93f8b871692801ac85a128
Sebacastro14 <sebacastro182@gmail.com>
2026-09-08 15:07:05 -0300

    Agrega clase Edificio con getSuperficieEdificio

---
+++ src/ejercicio2/Edificio.java
@@ -0,0 +1,20 @@
+package ejercicio2;
+
+public class Edificio {
+    private double superficieEdificio;
+
+    public Edificio() {
+    }
+
+    public Edificio(double superficieEdificio) {
+        this.superficieEdificio = superficieEdificio;
+    }
+
+    public double getSuperficieEdificio() {
+        return superficieEdificio;
+    }
+
+    public void setSuperficieEdificio(double superficieEdificio) {
+        this.superficieEdificio = superficieEdificio;
+    }
+}
